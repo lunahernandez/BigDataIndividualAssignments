4b059c5973c8d5cab77b87dd0b5438b6b6c518aa
lunahernandez <145288658+lunahernandez@users.noreply.github.com>
2024-10-09 16:56:33 +0100

    test_matrix_multiplication.py updated and JSON file added

--- code/python/matrix_multiplication.py
+++ code/python/matrix_multiplication.py
@@ -1,7 +1,20 @@
+import numpy as np
+
+
 def matrix_multiplication(a, b, n):
     c = [[0 for _ in range(n)] for _ in range(n)]
     for i in range(n):
         for j in range(n):
             for k in range(n):
                 c[i][j] += a[i][k] * b[k][j]
+    return c
+
+
+def matrix_multiplication_numpy_dot(a, b):
+    c = np.dot(a, b)
+    return c
+
+
+def matrix_multiplication_numpy_matmul(a, b):
+    c = np.matmul(a, b)
     return c
--- code/python/test_matrix_multiplication.py
+++ code/python/test_matrix_multiplication.py
@@ -1,7 +1,12 @@
 import csv
 import random
+import time
+
+from memory_profiler import profile
 import pytest
-from matrix_multiplication import matrix_multiplication
+from matrix_multiplication import (matrix_multiplication,
+                                   matrix_multiplication_numpy_dot,
+                                   matrix_multiplication_numpy_matmul)
 
 
 def read_matrix_sizes_from_csv(filename, delimiter=','):
@@ -13,13 +18,44 @@ def read_matrix_sizes_from_csv(filename, delimiter=','):
     return sizes
 
 
-matrix_sizes = read_matrix_sizes_from_csv('matrix_sizes.csv')
+matrix_sizes = read_matrix_sizes_from_csv('data/matrix_sizes.csv')
 
 
-@pytest.mark.parametrize("n", matrix_sizes)
-def test_matrix_multiplication_pedantic(benchmark, n):
+@pytest.mark.parametrize("n", matrix_sizes[:-2])
+@profile
+def test_matrix_multiplication_1(benchmark, n):
     a = [[random.random() for _ in range(n)] for _ in range(n)]
     b = [[random.random() for _ in range(n)] for _ in range(n)]
 
     benchmark.pedantic(matrix_multiplication, args=(a, b, n,),
                        iterations=100, rounds=10)
+
+
+@pytest.mark.parametrize("n", matrix_sizes[:-2])
+@profile
+def test_matrix_multiplication_2(benchmark, n):
+    a = [[random.random() for _ in range(n)] for _ in range(n)]
+    b = [[random.random() for _ in range(n)] for _ in range(n)]
+
+    benchmark.pedantic(matrix_multiplication, args=(a, b, n,),
+                       iterations=100, rounds=10, warmup_rounds=5)
+
+
+@pytest.mark.parametrize("n", matrix_sizes)
+@profile
+def test_matrix_multiplication_3(benchmark, n):
+    a = [[random.random() for _ in range(n)] for _ in range(n)]
+    b = [[random.random() for _ in range(n)] for _ in range(n)]
+
+    benchmark.pedantic(matrix_multiplication_numpy_dot, args=(a, b,),
+                       iterations=100, rounds=10, warmup_rounds=5)
+
+
+@pytest.mark.parametrize("n", matrix_sizes)
+@profile
+def test_matrix_multiplication_4(benchmark, n):
+    a = [[random.random() for _ in range(n)] for _ in range(n)]
+    b = [[random.random() for _ in range(n)] for _ in range(n)]
+
+    benchmark.pedantic(matrix_multiplication_numpy_matmul, args=(a, b,),
+                       iterations=100, rounds=10, warmup_rounds=5)
