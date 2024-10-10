3666fbc46e829e1ba94f435ed3bc331c1c8ebfef
lunahernandez <145288658+lunahernandez@users.noreply.github.com>
2024-10-10 15:41:24 +0100

    Parametrization in Java

--- code/java/matrixMultiplication/src/test/java/org/example/MatrixMultiplicationBenchmarking.java
+++ code/java/matrixMultiplication/src/test/java/org/example/MatrixMultiplicationBenchmarking.java
@@ -1,22 +1,27 @@
 package org.example;
 
 import org.openjdk.jmh.annotations.*;
-
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 @BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class MatrixMultiplicationBenchmarking {
 
+
     @State(Scope.Thread)
-    public static class Operands{
-        private final int n = 10;
-        private final double[][] a = new double[n][n];
-        private final double[][] b = new double[n][n];
+    public static class Operands {
+        public double[][] a;
+        public double[][] b;
+
+        @Param({"10", "100", "500"})
+        public int matrixSize;
 
         @Setup
         public void setup() {
+            int n = matrixSize;
+            a = new double[n][n];
+            b = new double[n][n];
             Random random = new Random();
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
@@ -25,16 +30,14 @@ public class MatrixMultiplicationBenchmarking {
                 }
             }
         }
-
     }
 
+
     @Benchmark
-    public void multiplication(Operands operands){
+    public void multiplication(Operands operands) {
         new MatrixMultiplication().execute(operands.a, operands.b);
     }
 }
-
-
 
 
 
